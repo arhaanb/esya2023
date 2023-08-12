d7fd696736fa3225b1496e6179e04c727647f327
AbhishekIIITD <abhishek21121@iiitd.ac.in>
2023-08-12 14:48:37 +0530

    changed the footer section as per the design

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,55 +2,70 @@
 	<main>
 		<br /><br />
 		<div class="navcont">
-			<div class="row">
-				<div class="five columns contact">
-					<h1 class="fancy">Join our community</h1>
-					<input type="text" name="" placeholder="Email" id="" />
-					<p>
-						*By checking the above box you are agreeing to receive email
-						communications from ESYA Inc. This can be changed at any time.
-						Please refer to our Privacy Policy and Terms of Use for more details
-						or Contact Us
-					</p>
-				</div>
-				<div class="six columns">
-					<div class="row">
-						<div class="four columns">
-							<h6 class="bold zero">Company</h6>
-							<h6 class="zero">About us</h6>
-							<h6 class="zero">Shop</h6>
-							<h6 class="zero">Blogs</h6>
+			<div class="navcontFooter">
+				<div class="colOne">
+					<div class="contactDetail">
+						<div class="iiitdLogoFooter">
+							<img
+								src="/iiitdLogoFooter.png"
+								class="iiitdLogoFooterImg"
+								alt="not loaded"
+							/>
 						</div>
-						<div class="four columns">
-							<h6 class="bold zero">Products</h6>
-							<h6 class="zero">Product Name</h6>
-							<h6 class="zero">Product Name</h6>
-							<h6 class="zero">Product Name</h6>
-							<h6 class="zero">Product Name</h6>
-							<h6 class="zero">Product Name</h6>
-						</div>
-						<div class="four columns">
-							<h6 class="bold zero">Customer Care</h6>
-							<h6 class="zero">FAQ</h6>
-							<h6 class="zero">Return Policy</h6>
-							<h6 class="zero">Promotional Terms & Conditions</h6>
-						</div>
+						<h3 style="display: inline">|</h3>
+						<h5 style="display: inline; font-weight: bold">
+							INDRAPRASTHA INSTITUTE OF INFORMATION TECHNOLOGY, DELHI
+						</h5>
+						<h5>
+							Okhla Industrial Area Phase 3, Near Govindpuri Metro Station
+							<br />New Delhi, Delhi, 110020
+						</h5>
 					</div>
 				</div>
-				<div class="one columns">
-					<h6 class="bold">Socials</h6>
+				<div class="colTwo">
+					<span class="contactDetail">
+						<img
+							src="/fonts/fill_phone.png"
+							class="mobileFilled"
+							alt="not loaded"
+						/>
+						<span>
+							<h6 style="display: inline; font-weight: bold">
+								&nbsp; &nbsp; Phone&nbsp;&nbsp;&nbsp;
+							</h6>
+							<p style="display: inline">
+								+91-011-71985300, <br />+91-011-26907400-7404
+							</p>
+						</span>
+					</span>
+					<span class="contactDetail">
+						<img
+							src="/fonts/mailIcon.png"
+							class="mailFilled"
+							alt="not loaded"
+						/>
+						<span>
+							<h6 style="display: inline; font-weight: bold">
+								&nbsp; &nbsp; Mail&nbsp;&nbsp;&nbsp;
+							</h6>
+							<p style="display: inline">info@iiitd.ac.in</p>
+						</span>
+					</span>
+					<span class="">
+						<img
+							src="/fonts/phoneIcon.png"
+							class="mobileFilled"
+							alt="not loaded"
+						/>
+						<span>
+							<h6 style="display: inline; font-weight: bold">
+								&nbsp; &nbsp; Fax&nbsp;&nbsp;&nbsp;
+							</h6>
+							<p style="display: inline">+91-11-26907405</p>
+						</span>
+					</span>
 				</div>
 			</div>
-
-			<div class="bottom fancy">
-				<h4>© ESYA</h4>
-				<div class="flexer">
-					<h4>Terms & Conditions</h4>
-					<img class="bop" src="/img/bop.svg" alt="" />
-					<h4>Privacy Policy</h4>
-				</div>
-			</div>
-
 			<br /><br />
 		</div>
 	</main>
@@ -66,6 +81,7 @@ main {
 	justify-content: space-between;
 	align-items: center;
 	margin-top: 1.5em;
+	font-family: 'Martel Sans', sans-serif;
 	h4 {
 		font-size: 1.35em;
 		margin: 0;
@@ -79,6 +95,42 @@ main {
 			width: 0.3em;
 		}
 	}
+}
+.navcontFooter {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-evenly;
+	padding-bottom: 1rem;
+}
+
+.colOne {
+	display: inline-block;
+}
+.colTwo {
+	display: flex;
+	flex-direction: column;
+	margin-left: 8rem;
+}
+.colTwo h6 {
+	margin-right: 2rem;
+}
+.contactDetail {
+	margin-bottom: 1rem;
+}
+
+.mobileFilled {
+	display: inline-block;
+	width: 1.625rem;
+	height: 2.685rem;
+	transform: translateY(0.5rem);
+	margin-right: 1rem;
+}
+.mailFilled {
+	display: inline-block;
+	width: 1.625rem;
+	height: 1.685rem;
+	transform: translateY(0.5rem);
+	margin-right: 1rem;
 }
 
 @media (max-width: 750px) {
@@ -95,6 +147,20 @@ main {
 	h6.bold {
 		margin-top: 1em;
 	}
+	.navcontFooter {
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: flex-start;
+	}
+	.colTwo{
+		margin-left:0;
+	}
+}
+.iiitdLogoFooter {
+	display: inline-block;
+	width: 5.75rem;
+	height: 3.065rem;
+	transform: translateY(0.5rem);
 }
 
 .line {
