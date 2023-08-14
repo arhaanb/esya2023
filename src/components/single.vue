95b568c888b489f2eac402b98192a699a518bbfd
arhaanb <arhaanb@gmail.com>
2023-08-14 16:35:35 +0530

    speakers

---
+++ src/components/single.vue
@@ -0,0 +1,85 @@
+<template>
+	<main>
+		<div class="bg">
+			<!-- <img src="/speakers/bruh.svg" alt="" /> -->
+			<div class="info">
+				<h1 class="bold zero">{{ name || null }}</h1>
+				<h3 class="zero">{{ designation || null }}</h3>
+				<div class="image">
+					<img :src="image || 'https://arhaanb.com/new_me.jpeg'" alt="" />
+					<div>
+						<h5 class="bold zero top">{{ time || null }}</h5>
+						<h5 class="bold zero btm">{{ location || null }}</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	props: ['name', 'designation', 'location', 'desc', 'time', 'image']
+}
+</script>
+
+<style lang="scss" scoped>
+.bg {
+	background: rgba(255, 255, 255, 0.25);
+	box-shadow: 0 8px 32px 0 rgba(228, 230, 255, 0.37);
+	backdrop-filter: blur(14px);
+	-webkit-backdrop-filter: blur(14px);
+	border-radius: 10px;
+	border: 1px solid rgba(255, 255, 255, 0.18);
+	padding: 2em;
+}
+.info {
+	h1 {
+		margin-bottom: 0em;
+		font-size: 2em;
+	}
+	h3 {
+		font-size: 1.75em;
+	}
+	.image {
+		display: flex;
+		justify-content: flex-start;
+		align-items: flex-end;
+		img {
+			width: 55%;
+			border-radius: 15px;
+			margin-top: 2em;
+		}
+		h5 {
+			text-transform: uppercase;
+			margin-left: 01.5em;
+			font-size: 1.25em;
+			&.btm {
+				margin-top: -0.2em;
+			}
+		}
+	}
+
+	* {
+		font-family: 'Manrope';
+	}
+	@media (max-width: 750px) {
+		h1 {
+			font-size: 2em;
+		}
+		h3 {
+			font-size: 1.25em;
+		}
+		.image {
+			img {
+				width: 60%;
+				margin-top: 2em;
+			}
+			h5 {
+				margin-left: 0.75em;
+				font-size: 1.1em;
+			}
+		}
+	}
+}
+</style>
--- src/components/speakers.vue
+++ src/components/speakers.vue
@@ -1,5 +1,54 @@
 <template>
-	<h1>Speakers section</h1>
+	<main>
+		<h1 class="center akira titled">SPEAKERS</h1>
+		<div class="grid">
+			<div class="speaker">
+				<Single
+					name="Arhaan Bahadur"
+					designation="Designer"
+					time="9:00 PM"
+					location="C-102, LHC"
+					image="https://arhaanb.com/yo.jpg"
+				/>
+			</div>
+
+			<div class="speaker">
+				<Single
+					name="Name 1"
+					designation="Designer"
+					time="10:00 PM"
+					location="C-102, LHC"
+					image="https://arhaanb.com/new_me.jpeg"
+				/>
+			</div>
+		</div>
+	</main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.grid {
+	display: grid;
+	grid-gap: 3em;
+	grid-template-columns: repeat(3, 1fr);
+}
+
+.speaker {
+	position: relative;
+}
+
+.titled {
+	margin-bottom: 0.75em;
+	font-size: 6em;
+}
+@media (max-width: 750px) {
+	.grid {
+		display: grid;
+		grid-gap: 3em;
+		grid-template-columns: repeat(1, 1fr);
+	}
+	.titled {
+		font-size: 200%;
+		margin-bottom: 1.5em;
+	}
+}
+</style>
